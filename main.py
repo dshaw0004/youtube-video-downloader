ccfcd898e3578d564d91666f1e84c21f9b2568be
dshaw0004 <dipankarshaw692@gmail.com>
2023-02-11 21:43:08 +0530

    few comments are added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# require modules
 from tkinter import *
 from tkinter.filedialog import askdirectory
 from tkinter.messagebox import showerror, showinfo
@@ -5,44 +6,37 @@ from tkinter.messagebox import showerror, showinfo
 from pytube import YouTube
 
 
-def YoutubeDownload(video_url, path, format, res):
-    video = YouTube(video_url)
+def download():  # this fuction handle all the logic
+    path = askdirectory(title='Select Folder')
+    videoLink = link.get()
+    downloadType = fileType.get()
+    resolution = res.get()
+    video = YouTube(videoLink)
 
     try:
-        if format == "audio":
+        if downloadType == "audio":
             file = video.streams.filter(only_audio=True).first()
         else:
-            file = video.streams.filter(resolution=res).first()
+            file = video.streams.filter(resolution=resolution).first()
 
         file.download(path)
 
     except:
         showerror("error", "Unable to download video at this time!")
 
-    return (["download complete", f"{format} downloaded!"])
+    showinfo("download complete", f"{downloadType} downloaded!")
 
 
-bgColor = "#aaddff"
-
-
-def processData():
-    path = askdirectory(title='Select Folder')
-    videoLink = link.get()
-    downloadType = fileType.get()
-    resolution = res.get()
-    returnedData = YoutubeDownload(
-        videoLink, fr"{path}", downloadType, resolution)
-    showinfo(returnedData[0], returnedData[1])
-
-
+bgColor = "#aaddff"  # backgrond color of the app
+# Initializing tkinter
 root = Tk()
 root.geometry("350x250")
 root.resizable(0, 0)
 root.configure(bg=bgColor)
-root.iconbitmap("./icon.ico")
+# root.iconbitmap("./icon.ico") #this does not work properly
 root.title("YTdownloader")
 
-
+# Widget implementation
 Label(root, text="Youtube Downloader",
       bg=bgColor, font="sarif 15 bold").pack(pady=7)
 
@@ -51,32 +45,34 @@ frame = Frame(root, padx=10, pady=20,
               bg=bgColor, height=150
               )
 frame.pack(fill="both")
-frame.columnconfigure(0, weight=2)
-frame.columnconfigure(1, weight=2)
 
-
-link = StringVar()
+link = StringVar()  # link of the video will be saved here
 Entry(frame, textvariable=link).place(
-    in_=frame, anchor="c", relx=.5, rely=0.0, relheight=0.3, relwidth=0.9)
+    in_=frame, anchor="c", relx=.5,
+    rely=0.0, relheight=0.3, relwidth=0.9)
+
 
 Label(text="filetype", bg=bgColor).place(
-    in_=frame, anchor="c", relx=.25, rely=.375)
-fileType = StringVar()
-fileType.set("video")
+    in_=frame, anchor="c", relx=.25, rely=.325)
+
+fileType = StringVar()  # type of download video or audio
+fileType.set("video")  # setting the default type to video
+
 OptionMenu(frame, fileType, *["video", "audio"]
-           ).place(in_=frame, anchor="c", relx=.25, rely=.65)
+           ).place(in_=frame, anchor="c", relx=.25, rely=.575)
 
 Label(text="resolution", bg=bgColor).place(
-    in_=frame, anchor="c", relx=.75, rely=.375)
+    in_=frame, anchor="c", relx=.75, rely=.325)
 
-res = StringVar()
+res = StringVar()  # video quality of the video will stored here
 resList = ["144p", "240p", "360p", "480p", "720p", "1080p"]
-res.set(resList[2])
+res.set(resList[2])  # setting the deafult video quality to 360p
+
 OptionMenu(frame, res, *resList
-           ).place(in_=frame, anchor="c", relx=.75, rely=.65)
+           ).place(in_=frame, anchor="c", relx=.75, rely=.575)
 
 
-Button(root, command=processData, text="download").pack(pady=5)
-
+Button(root, command=download, text="download").place(
+    in_=frame, anchor="c", relx=.5, rely=.9, relheight=0.3)
 
 root.mainloop()
